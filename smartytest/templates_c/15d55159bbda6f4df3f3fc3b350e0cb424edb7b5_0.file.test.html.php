1db6506d5be0a6fa751325d2594661a284ac34e2
pengsimin <18253590634@163.com>
2017-11-10 18:00:06 +0800

    smarty安装测试demo

---
+++ smartytest/templates_c/15d55159bbda6f4df3f3fc3b350e0cb424edb7b5_0.file.test.html.php
@@ -0,0 +1,45 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-11-10 09:59:51
+  from "C:\workspace\PHP-utils\smartytest\templates\test.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5a0578973235e9_39569500',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '15d55159bbda6f4df3f3fc3b350e0cb424edb7b5' => 
+    array (
+      0 => 'C:\\workspace\\PHP-utils\\smartytest\\templates\\test.html',
+      1 => 1510306548,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5a0578973235e9_39569500 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<!--
+  smarty模板使用的安装测试demo
+-->
+<html>
+  <head>
+    <title>smarty测试</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <div>测试smarty</div>
+    <p><?php echo $_smarty_tpl->tpl_vars['test']->value;?>
+</p>
+  </body>
+</html>
+
+
+
+<?php }
+}
