4f177194af0d690b4f0a82c1cec57467bf911382
pengsimin <18253590634@163.com>
2017-11-13 16:33:44 +0800

    smarty的一些方法的使用学习练习

--- exercise/smarty.php
+++ exercise/smarty.php
@@ -15,9 +15,40 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Smarty/Smarty.class.php';
 $smarty = new Smarty();
 $smarty -> template_dir = $_SERVER['DOCUMENT_ROOT']."/smartytest/templates"; //模板存放目录 
 $smarty -> compile_dir = $_SERVER['DOCUMENT_ROOT']."/smartytest/templates_c"; //模板存放目录 
-$smarty -> left_delimiter = "{{"; //左定界符 
-$smarty -> right_delimiter = "}}"; //右定界符 
+$smarty -> left_delimiter = "{"; //左定界符 
+$smarty -> right_delimiter = "}"; //右定界符 
+
+//变量
 $smarty -> assign('test','改了一下限定符号'); 
+
+//数组变量
+$contacts = array('fax'=>'555-222-9876',
+                   'email'=>'fhrj@fhdj.com',
+                   'other'=>array( 'a'=>'aa','b'=>'bb'));
+$smarty->assign('contacts', $contacts);
+
+//数组下标
+$contacts_back = array( '1057585926','18253590634');
+$smarty->assign('contacts_back',$contacts_back);
+
+//对象
+$obj = new stdClass();
+$obj->name = '张三';
+$obj->age = '24';
+$smarty->assign('obj',$obj);
+
+//变量修饰器
+$abc = 'abc';
+$smarty->assign('abc',$abc);
+
+//第一个首字母大写capitalize
+$smarty->assign('articleTitle', 'next x-men film, x3, delayed.');
+
+$smarty->assign('articleTitlea', 'Smokers are Productive, but Death Cuts Efficiency.');
+
+$smarty->assign('to',10);
+$smarty->assign('start',8);
+
 $smarty -> display('test.html'); 
 
 
